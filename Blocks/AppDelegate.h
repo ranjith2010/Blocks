5cf55b2388ad007f90493154a3feb8e3bf3e7546
Ranjith <ranjith.kumar@zippr.in>
2014-12-24 14:31:46 +0530

    Initial Commit

---
+++ Blocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocks
+//
+//  Created by Ranjith on 24/12/14.
+//  Copyright (c) 2014 Zippr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
