a5bf638eab4e363902c0c086271b1c02085698c5
Ranjith <ranjith.kumar@zippr.in>
2014-12-30 13:37:31 +0530

    ThirdClass File attached

---
+++ Blocks/ThirdClass.h
@@ -0,0 +1,13 @@
+//
+//  ThirdClass.h
+//  Blocks
+//
+//  Created by Ranjith on 30/12/14.
+//  Copyright (c) 2014 Zippr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdClass : NSObject
+
+@end
---
+++ Blocks/ThirdClass.m
@@ -0,0 +1,13 @@
+//
+//  ThirdClass.m
+//  Blocks
+//
+//  Created by Ranjith on 30/12/14.
+//  Copyright (c) 2014 Zippr. All rights reserved.
+//
+
+#import "ThirdClass.h"
+
+@implementation ThirdClass
+
+@end
